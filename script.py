28e514b254d10ba986875af7331c7674a9fb2bb0
joewragg <joewragg@protonmail.com>
2018-04-17 17:18:18 +0100

    Commit

--- script.py
+++ script.py
@@ -13,9 +13,6 @@ from scipy.spatial import distance
 from sympy.solvers import solve
 from sympy import Symbol
 
-#Var
-
-
 #Constants
 posGES = np.array([-2368.8, 4881.1, -3342.0])
 posAES = np.array([-1293.0, 6238.3, 303.5])
@@ -23,17 +20,9 @@ c = 299792458/1000#km/s
 iterationConstant = 8000
 alt = 10668*1e-3
 
+#function to parse data from satelite report file and inmarsat csv file
 def getData(Time = 3):
-	x = []
-	y = []
-	z = []
-	vx = []
-	vy = []
-	vz = []
-	lat = []
-	lon = []
-	dateSat = []
-	alt = []
+	x,y,z,vx,vy,vz,lat,lon,dateSat,alt = ([] for i in range(10))
 	#Grab InmarSat Data
 	data = pd.read_csv("inmarsat.csv", usecols=[0,8,25,27])
 	data.rename(columns={'Time':'Date', 'Frequency Offset (Hz)': 'BFO', 'Burst Timing Offset (microseconds)': 'BTO', 'Channel Type': 'ChType'}, inplace=True)
@@ -44,12 +33,9 @@ def getData(Time = 3):
 	#Grab report data
 	for i, line in enumerate(lines):
 		#if i <=1:
-		#       print(line, "\n\n")
 		if "Nov" in line:
 			print(line)
 			lines.pop(i)
-			#May not be working not sure if git took out rest of file?
-			#print(lines[17868])
 		if i!=0:
 			dateSat.append(datetime.strptime(line.split("  ")[0], "%d %b %Y %H:%M:%S.%f"))
 			x.append(line.split()[4])
@@ -76,7 +62,6 @@ def getData(Time = 3):
 	data = data[pd.notnull(data['BTO'])]
 	data = data.reset_index(drop=True)
 	for i in range(len(data)):
-		print(data['Date'][i])
 		for j in range(len(dateSat)):
 			if abs(dateSat[j]-data['Date'][i])<=timedelta(0,0,0,50):
 				dateSatd.append(dateSat[j])
@@ -124,13 +109,7 @@ def getData(Time = 3):
 	data.BFO = data.BFO.astype(float)
 	return data
 
-def inputR(inputText, wantedTextList):
-	inp = False
-	while inp == False:
-		string = raw_input(inputText)
-		if string in wantedTextList:inp = True
-	return string 
-
+# returns bias given data
 def getBias(posSat):
 	distSatGES = np.linalg.norm(posSat-posGES, axis = 1)
 	distSatAES = np.linalg.norm(posSat-posAES, axis = 1)
@@ -150,8 +129,7 @@ def getBias(posSat):
 			meanBiasT = np.mean(biasT)
 			meanBiasT = -0.495679
 			meanBiasR=meanBiasT
-			print(meanBiasR)
-			print(meanBiasT)
+			print("The bias used is: ", meanBiasT, "s")
 		if data["Date"][i]<=datetime(2014,3,7,16,29,52,406000):#preTakeOff
 			if data['ChType'][i]=="R-Channel RX":
 				bias.append(biasR[biasRn])
@@ -165,6 +143,7 @@ def getBias(posSat):
 	bias = pd.Series(bias)
 	return bias, distSatGES
 
+#gives indexes for data matching the arc dates 
 def getArcDates():
 	arcDate = []
 	arcIndexes = [] 			
@@ -181,6 +160,7 @@ def getArcDates():
 				arcIndexes.append(j)	
 	return arcIndexes
 
+#converts latitude and longitude to earth centered earth fixed coordinates
 def lla_to_ecef(lat, lon, alt):
     import pyproj
     ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
@@ -188,24 +168,32 @@ def lla_to_ecef(lat, lon, alt):
     x, y, z = pyproj.transform(lla, ecef, lon, lat, alt, radians=False)
     return x, y, z
 
-inString = inputR("Get data? yes/no: ", ["yes", "no"])
-if inString =="yes":
-	data = getData()
-	arcDates = getArcDates()
-	data["BTO"][arcDates[0]] = data.iloc[arcDates[0]].loc["BTO"]-4600
-	data["BTO"][arcDates[6]] = data.iloc[arcDates[6]].loc["BTO"]-4600
-	data.to_csv("Data.csv")
-if inString =="no":
-	data = pd.read_csv("Data.csv")
-	data = data.drop(['Unnamed: 0'], axis=1)
-	data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d %H:%M:%S.%f')
-	data['DateSat'] = pd.to_datetime(data['DateSat'], format='%Y-%m-%d %H:%M:%S.%f')
-	arcDates = getArcDates()
+#converts ecef to lat lon coords
+def ecef_to_lla(x, y, z):
+    import pyproj
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+    lon, lat, alt = pyproj.transform(ecef, lla, x, y, z, radians=False)
+    return lat, lon, alt
+
+#Main code start
+pd.options.mode.chained_assignment = None
+print("getting data...")
+data = getData()
+print("done")
+arcDates = getArcDates()
+data["BTO"][arcDates[0]] = data.iloc[arcDates[0]].loc["BTO"]-4600
+data["BTO"][arcDates[6]] = data.iloc[arcDates[6]].loc["BTO"]-4600
+data.to_csv("Data.csv")
+data = pd.read_csv("Data.csv")
+data = data.drop(['Unnamed: 0'], axis=1)
+data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d %H:%M:%S.%f')
+data['DateSat'] = pd.to_datetime(data['DateSat'], format='%Y-%m-%d %H:%M:%S.%f')
+arcDates = getArcDates()
 
 #Some more data assignment
 data["bias"], distSatGES = getBias(data.as_matrix(['x','y','z']))
 data["Dist"] = (0.5*c*((data["BTO"].values*1e-6)-data["bias"].values)) - distSatGES 
-pd.options.mode.chained_assignment = None
 deltaSatAFC = np.zeros(len(data))
 deltaFBias = np.full(len(data), 152.5)
 data["deltaSatAFC"] = pd.Series(np.asarray(deltaSatAFC))
@@ -244,13 +232,13 @@ def drawLine(Name, originLat, originLon, destLat, destLon, color):
 	ls.coords = [(originLon, originLat), (destLon, destLat)]
 	return 1
 
-#BTO Analysis
 kml = simplekml.Kml()
-drawPoint("Satposition", np.mean(data["Lon"].values), np.mean(data["Lat"].values),357860)
+drawPoint("Satposition", np.mean(data["Lat"].values), np.mean(data["Lon"].values),357860)
 arcIndexes = arcDates
 arcNo = 0
 
 circles = []
+#draw arcs
 for i in range(len(data)):
 	arcNo = arcNo+1
 	a = data["Dist"][i]
@@ -265,8 +253,8 @@ for i in range(len(data)):
 	datetime(2014,3,7,16,29,52,406000)
 	radius = radius*1000
 	circles.append(drawCircle("Arc"+str(arcNo), data["Lat"][i], data["Lon"][i], radius, simplekml.Color.white))
-	print(str(radius/1000)+"km")
 
+#give a table of delta F comps
 def getFComp(posPl, speed, posSat):
 	v = v/1000
 	#sat = [0,64.5,35786*1e3]
@@ -279,6 +267,7 @@ def getFComp(posPl, speed, posSat):
 	deltaFComp = Fup*(((c+vs)/c)-1)
 	return deltaFComp
 
+#find path between arcs given a direction north or south  
 def findShortest(name, radius, origin, alt, circle, direction):
 	distArr = []
 	circle = circle.to_lat_lon()
@@ -304,14 +293,13 @@ def drawPath(origin, dest, time, no):
 	drawLine("Path"+str(no), origin.latitude, origin.longitude, dest.latitude, dest.longitude, simplekml.Color.red)
 	return 1 
 
-def getDest(origin, lastTime, time, bearing):
-	deltaT = abs(lastTime-time).total_seconds()
-	radius = speed*deltaT
+#get destination given origin times and breaing
+def getDest(origin, lastTime, time, bearing, radius):
 	dest = VincentyDistance(kilometers=radius*1e-3).destination(origin, bearing)
 	return dest
 
 alt = alt*1000
-speed = 243.322#cruising Speed
+speed = 231.5#450knots
 time = datetime(2014,3,7,17,6,43)
 origin = geopy.Point(2.7, 101.7, 0)
 dest = geopy.Point(5.27, 102.79, alt)
@@ -320,7 +308,9 @@ drawPath(origin, dest, time, 1)
 origin = dest
 lastTime = time
 time = datetime(2014, 3, 7, 17, 21, 13)
-dest = getDest(origin, lastTime, time, 25)
+deltaT = abs(lastTime-time).total_seconds()
+radius = speed*deltaT
+dest = getDest(origin, lastTime, time, 25, radius)
 drawPath(origin, dest, time, 2)
 
 origin = dest
@@ -341,25 +331,32 @@ lastTime = time
 time = data["Date"][0]
 deltaT = abs(lastTime-time).total_seconds()
 radius = speed*deltaT
-print(data)
 destArr = []
 dest = findShortest("test", radius, origin, alt, circles[0], "North")	
 destArr.append(dest)
 firstArcPos = dest
 drawPath(origin, dest, time, 5)
 
-speed = 243.322#cruising Speed
-#speed = 257.322#500knots
+speed = 231.5#450knots
+#draw paths
 for i in range(0,6):
-	origin = dest
-	deltaT = abs(data["Date"][i+1]-data["Date"][i]).total_seconds()
-	radius = speed*deltaT
-	print(data["Date"][i+1].hour)
-	time = data["Date"][i+1]
-	dest = findShortest("test", radius, origin, alt, circles[i+1], "South")
-	destArr.append(dest)
-	drawPath(origin, dest, time, 5+i)
-print('\n')
+    origin = dest
+    deltaT = abs(data["Date"][i+1]-data["Date"][i]).total_seconds()
+    radius = speed*deltaT
+    time = data["Date"][i+1]
+    dest = findShortest("test", radius, origin, alt, circles[i+1], "South")
+    destArr.append(dest)
+    drawPath(origin, dest, time, 5+i)
+    #drawCircle('test', origin.latitude, origin.longitude, radius, simplekml.Color.green)
+    #draw green circle for example
+
+origin = dest
+lastTime = time
+time = lastTime-timedelta(0,120) 
+deltaT = abs(lastTime-time).total_seconds()
+radius = speed*deltaT
+dest = getDest(origin, lastTime, time, 7.70, radius)
+drawPath(origin, dest, time, 11)
 
 #BFO Analysis
 deltaFDown = []
@@ -375,10 +372,11 @@ data.deltaFDown = data.deltaFDown.astype(float)
 
 maxAlt = 10668#35kfeet
 glideDist = maxAlt*16.995
-print(glideDist)
-drawCircle("Glide", destArr[6].latitude, destArr[6].longitude, glideDist, simplekml.Color.white) 
+drawCircle("Glide", dest.latitude, dest.longitude, glideDist, simplekml.Color.white) 
 drawPoint("Their location", -35.6, 92.8, 0)
+print(data)
 
+#save to kml file ready for importing to google earth
 kml.save("Flight.kml")
-#Arc0
+#save data
 data.to_csv("FinalData.csv")
